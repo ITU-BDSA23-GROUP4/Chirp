0399d44d20a676c81eee388e47caeaf64a2b61cd
Lukas Andersson <lukan@itu.dk>
2023-08-31 16:02:37 +0200

    Created two methods for reading and printing from a CSV file to the CLI in a structured format.
    Co-authored-by: Marius <mariu@itu.dk> and Oliver <oash@itu.dk>

--- Chirp/Program.cs
+++ Chirp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+string path = "";
+
+void saveToFile() {
+
+}
+
+void readFromFile() {
+    try {
+        var sr = new StreamReader(path);
+        while(sr.Peek() >= 0) {
+            string line = sr.ReadLine();
+            if (line.StartsWith("Author,Message,Timestamp"))
+                continue;
+            printToCLI(line.Split(","));
+        }
+
+    } catch {
+        Console.WriteLine("Can't read from file");
+    }
+}
+
+void printToCLI(String[] line) {
+    Console.WriteLine("User" + line[0]);
+    var message = line[1].Replace("/comma/", ",");
+    Console.WriteLine(message);
+    Console.WriteLine("At time: " + line[2]);
+
+void readFromCLI() {
+
+}
