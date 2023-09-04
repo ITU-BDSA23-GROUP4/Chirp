d4261535265cf9a1c2fbbcdda040e4ace42e27dd
Lukas Andersson <lukan@itu.dk>
2023-09-04 15:15:21 +0200

    Added readFromCLI and saveToFile methods.
    Co-authored-by: Marius <mariu@itu.dk>, Anna <annaj@itu.dk>, Oliver <oash@itu.dk>, Niels <nfab@itu.dk>

--- Chirp/Program.cs
+++ Chirp/Program.cs
@@ -1,10 +1,14 @@
-﻿using System.IO;
-using System.Text.RegularExpressions;
+﻿using System;
 
-string path = "";
+string path = "ccirp_cli_db.csv";
 
-void saveToFile() {
-
+void saveToFile(string line) {
+    try {
+        var sw = new StreamWriter(path);
+        sw.WriteLine(line);
+    } catch {
+        Console.WriteLine("Can't write to file");
+    }
 }
 
 void readFromFile() {
@@ -12,6 +16,7 @@ void readFromFile() {
         var sr = new StreamReader(path);
         while(sr.Peek() >= 0) {
             string line = sr.ReadLine();
+            // First line contains headers and not data. Therfore we skip it
             if (line.StartsWith("Author,Message,Timestamp"))
                 continue;
             printToCLI(line.Split(","));
@@ -27,7 +32,32 @@ void printToCLI(String[] line) {
     var message = line[1].Replace("/comma/", ",");
     Console.WriteLine(message);
     Console.WriteLine("At time: " + line[2]);
+}
 
 void readFromCLI() {
+    Console.WriteLine("Please enter your message");
+    string message = Console.ReadLine().Replace(",", "/comma/");
+    string author = Environment.UserName;
+    // https://www.delftstack.com/howto/csharp/how-to-get-the-unix-timestamp-in-csharp/
+    TimeSpan epochTicks = new TimeSpan(new DateTime(1970, 1, 1).Ticks);
+    TimeSpan unixTicks = new TimeSpan(DateTime.Now.Ticks) - epochTicks;
+    Int32 time = (Int32) unixTicks.TotalSeconds;
+    //saveToFile(author + "," + message + "," + time);
+    saveToFile("TEst");
+}
 
+Console.WriteLine("Possible commands: post, read, quit");
+
+string cmd = Console.ReadLine();
+while(!cmd.Equals("quit")) {
+    if (cmd.Equals("post")) {
+        readFromCLI(); 
+    } else if (cmd.Equals("read")) {
+        readFromFile();
+    } else if (cmd.Equals("quit")) {
+        break;
+    } else {
+        Console.WriteLine("Possible commands: post, read, quit");
+    }
+    cmd = Console.ReadLine();
 }
