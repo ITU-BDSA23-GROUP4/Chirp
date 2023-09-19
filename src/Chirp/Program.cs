25dea054637a6d15f25e3a0c6d374500f0d135b3
Marius Thomsen <mariu@itu.dk>
2023-09-19 11:01:23 +0200

    Reolocated Cheep

--- src/Chirp/Program.cs
+++ src/Chirp/Program.cs
@@ -7,7 +7,7 @@ using System.Xml.Serialization;
 using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
-
+using CheepNS;
 class CLI
 {
     static string path = "ccirp_cli_db.csv";   //The file where we store our cheeps
@@ -69,13 +69,3 @@ public class CheepOptions
 public class ReadOptions {}
 
 
-//Author,Message,Timestamp
-public record Cheep
-{
-    [Index(0)]
-    public string Author { get; set; }
-    [Index(1)]
-    public string Message { get; set; }
-    [Index(2)]
-    public long Timestamp { get; set; }
-}
--- src/Chirp/Userinterface.cs
+++ src/Chirp/Userinterface.cs
@@ -1,17 +1,18 @@
 using System;
-
+using CheepNS;
 public static class Userinterface<T> {
  
     public static void PrintCheeps(IEnumerable<Cheep> cheeps ){
-        foreach (var Cheep in cheeps)    //Setting the layour for all cheeps
+        foreach (var cheep in cheeps)    //Setting the layour for all cheeps
         {
-        Console.WriteLine("User: " + Cheep.Author);
-        var message = Cheep.Message.Replace("/comma/", ","); //Replaces what we did earlier, for a cleaner output
-        Console.WriteLine(message);
+        Console.WriteLine(cheep.ToString());
+        // Console.WriteLine("User: " + Cheep.Author);
+        // var message = Cheep.Message.Replace("/comma/", ","); //Replaces what we did earlier, for a cleaner output
+        // Console.WriteLine(message);
 
-        // Creates time obejct from unix time stamp and prints it in local time zone
-        DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(Cheep.Timestamp);
-        Console.WriteLine("At time: " + time.LocalDateTime +"\n");
+        // // Creates time obejct from unix time stamp and prints it in local time zone
+        // DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(Cheep.Timestamp);
+        // Console.WriteLine("At time: " + time.LocalDateTime +"\n");
         }
        
     } 
