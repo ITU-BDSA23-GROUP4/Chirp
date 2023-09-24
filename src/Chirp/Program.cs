17cb2c353a58c67628c63dcb3f8eb08e4265620d
Niels Christian Skov Faber <nfab@itu.dk>
2023-09-24 21:22:13 +0200

    Revert "Temporary fix"
    This reverts commit 09ad17ccfdd933d77cd0b45542c86fe23d84e8b3.

--- src/Chirp/Program.cs
+++ src/Chirp/Program.cs
@@ -1,49 +1,32 @@
 ﻿using CommandLine;
 using CheepNS;
+using ReadOptionsNS;
+using CheepOptionsNS;
 
 namespace CLINS
 {
     public class CLI
     {
-        static string path = "ccirp_cli_db.csv";   //The file where we store our cheeps¨
-        static CSVDatabase<Cheep> DB;
+        static string path = "src/Chirp/chirp_cli_db.csv";   //The file where we store our cheeps¨
+        static CSVDatabase<Cheep>? DB;
 
         static void Main(string[] args)
         {
+
             DB = CSVDatabase<Cheep>.GetCSVDatabase();    //Initializing the database
-            
-            //temporary code
-            if (File.Exists("src/Chirp/chirp_cli_db.csv")) {
-            DB.SetPath("src/Chirp/chirp_cli_db.csv");
-            }
-            else {
-                DB.SetPath("chirp_cli_db.csv");
-            }
-           
-            //DB.SetPath(path);
+            DB.SetPath(path);
 
             Parser.Default.ParseArguments<CheepOptions, ReadOptions>(args)
             .WithParsed<CheepOptions>(result =>
-            {
-                if (result.MessageOption != null)    //Will see which commands the user are requesting, and then create a cheep from that
-                    try
-                    {
-                        DB.SaveToFile(ConstructCheep(result.MessageOption));
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-
-                if (result.MessageValue != null)
-                    try
-                    {
-                        DB.SaveToFile(ConstructCheep(result.MessageValue));
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
+            {  
+                try
+                {
+                    DB.SaveToFile(ConstructCheep(result.MessageValue));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             })
             .WithParsed<ReadOptions>(result =>
             {
@@ -69,21 +52,4 @@ namespace CLINS
             return new Cheep { Author = author, Message = message, Timestamp = time.ToUnixTimeSeconds() };
         }
     }
-    //End of class CLI
-
-
-    [Verb("cheep", HelpText = "Post a cheep.")]
-    public class CheepOptions
-    {
-        [Option('m', "message", HelpText = "Cheep message.")]
-        public string MessageOption { get; set; }
-
-        [Value(0, HelpText = "Cheep message.")]
-        public string MessageValue { get; set; }
-    }
-
-
-    [Verb("read", HelpText = "Read all cheeps.")]
-    public class ReadOptions { }
-
 }
