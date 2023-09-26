f8c27b30bb35bcaa182267c621dce635c336b22c
Marius Thomsen <mariu@itu.dk>
2023-09-26 13:43:50 +0200

    Revert "Merge branch 'main' of https://github.com/ITU-BDSA23-GROUP4/Chirp"
    This reverts commit 0887525a449d55ef023e8764a6db9d027f247d7b, reversing
    changes made to e005620f21c04b79790da5f7d1aabbdc25d8bd9b.

--- src/Chirp.CSVDBService/WebServiceDatabase.cs
+++ src/Chirp.CSVDBService/WebServiceDatabase.cs
@@ -2,23 +2,25 @@ using CheepNS;
 
 public class WebS
 {
-    static string path = "../Chirp/chirp_cli_db.csv";
-    static CSVDatabase<Cheep>? DB;
+    //static string path = "../Chirp/chirp_cli_db.csv";
+    //static CSVDatabase<Cheep>? DB;
 
     static void Main(String[] args)
     {
-        DB = CSVDatabase<Cheep>.GetCSVDatabase();
-        DB.SetPath(path);
+        //DB = CSVDatabase<Cheep>.GetCSVDatabase();
+        //DB.SetPath(path);
 
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
         app.MapGet("/cheeps", async () => {
-            return await DB.ReadFromFileAsync();
+            return "Cheep";
+            //return await DB.ReadFromFileAsync();
         });
 
         app.MapPost("/cheep", async (Cheep cheep) => { 
-            await DB.SaveToFileAsync(cheep);
+            return "Cheeps";
+            //await DB.SaveToFileAsync(cheep);
         });
 
         app.Run();
