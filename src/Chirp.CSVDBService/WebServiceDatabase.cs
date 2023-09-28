ba938e0d4eca45e913479fc450f2ae1789f158e4
Marius Thomsen <mariu@itu.dk>
2023-09-28 14:53:33 +0200

    Azure issues

--- src/Chirp.CSVDBService/WebServiceDatabase.cs
+++ src/Chirp.CSVDBService/WebServiceDatabase.cs
@@ -1,28 +1,39 @@
-using CheepNS;
+using System.Globalization;
+//DB = CSVDatabase<Cheep>.GetCSVDatabase();
+//DB.SetPath(path);
 
-public class WebS
-{
-    //static string path = "../Chirp/chirp_cli_db.csv";
-    //static CSVDatabase<Cheep>? DB;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-    static void Main(String[] args)
+app.MapGet("/cheeps",  () => {
+    return "Cheep";
+    //return await DB.ReadFromFileAsync();
+});
+
+app.MapPost("/cheep", (Cheep cheep) => { 
+    return "Cheeps";
+    //await DB.SaveToFileAsync(cheep);
+});
+
+app.Run();
+
+public record Cheep
     {
-        //DB = CSVDatabase<Cheep>.GetCSVDatabase();
-        //DB.SetPath(path);
+        //[Index(0)]
+        public string Author { get; set; }
+        //[Index(1)]
+        public string Message { get; set; }
+        //[Index(2)]
+        public long Timestamp { get; set; }
+        public override string ToString()
+        {
+            var printMessage = Message.Replace("/comma/", ","); //Replaces what we did earlier, for a cleaner output
+                                                                // Changing cutlture date and time format from this source: https://code-maze.com/csharp-datetime-format/
+            return $"{Author} @ {CreateTimeString(Timestamp)}: {printMessage}";
+        }
 
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
-
-        app.MapGet("/cheeps", async () => {
-            return "Cheep";
-            //return await DB.ReadFromFileAsync();
-        });
-
-        app.MapPost("/cheep", async (Cheep cheep) => { 
-            return "Cheeps";
-            //await DB.SaveToFileAsync(cheep);
-        });
-
-        app.Run();
+        private string CreateTimeString(long TimeStamp) {
+            DateTimeOffset utcTime = DateTimeOffset.FromUnixTimeSeconds(Timestamp);
+            return utcTime.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss", new CultureInfo("sw-SW"));
+        }
     }
-}
