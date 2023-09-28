d3cb8fdff5e1e8d4f4d48645e842dea0084e31a4
Marius Thomsen <mariu@itu.dk>
2023-09-28 15:29:03 +0200

    Azure deploy ment should work now. Fixed dependencies so that azure launches from the right project for not the cheep record is a dublicate it should be moved to to a seperate folder so that all project can see it. and there are no dublicates Co-authored-by: Lukas lukan@itu.dk

--- src/Chirp.CSVDBService/WebServiceDatabase.cs
+++ src/Chirp.CSVDBService/WebServiceDatabase.cs
@@ -1,34 +1,37 @@
 using System.Globalization;
-//DB = CSVDatabase<Cheep>.GetCSVDatabase();
-//DB.SetPath(path);
+
+
+string path = "chirp_cli_db.csv";
+CSVDatabase<Cheep>? DB = CSVDatabase<Cheep>.GetCSVDatabase();
+DB.SetPath(path);
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/cheeps",  () => {
-    return "Cheep";
-    //return await DB.ReadFromFileAsync();
+app.MapGet("/cheeps", async () => {
+    // return "Cheep";
+    return await DB.ReadFromFileAsync();
 });
 
-app.MapPost("/cheep", (Cheep cheep) => { 
-    return "Cheeps";
-    //await DB.SaveToFileAsync(cheep);
+app.MapPost("/cheep", async (Cheep cheep) =>  { 
+    // return "Cheeps";
+    await DB.SaveToFileAsync(cheep);
 });
 
 app.Run();
 
 public record Cheep
     {
-        //[Index(0)]
+        // [Index(0)]
         public string Author { get; set; }
-        //[Index(1)]
+        // [Index(1)]
         public string Message { get; set; }
-        //[Index(2)]
+        // [Index(2)]
         public long Timestamp { get; set; }
         public override string ToString()
         {
             var printMessage = Message.Replace("/comma/", ","); //Replaces what we did earlier, for a cleaner output
-                                                                // Changing cutlture date and time format from this source: https://code-maze.com/csharp-datetime-format/
+                                                                //Changing cutlture date and time format from this source: https://code-maze.com/csharp-datetime-format/
             return $"{Author} @ {CreateTimeString(Timestamp)}: {printMessage}";
         }
 
