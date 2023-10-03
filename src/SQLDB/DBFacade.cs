adef5a44185bdcf507549da4cdbfe2fda9493cae
Oliver <oash@itu.dk>
2023-10-03 13:41:45 +0200

    Made DBFacade and SQLDatabase In this commit the database has been created and the files has been put in data folder. The facade is also made so that i creates a connection to the SQL database
    Co-authored-by: Lukas <lukan@itu.dk>

---
+++ src/SQLDB/DBFacade.cs
@@ -0,0 +1,23 @@
+using Microsoft.Data.Sqlite;
+
+namespace SQLDB {
+    public class  DB {
+        
+        private static readonly DB instance = new DB();
+        private static SqliteConnection? connection;
+        
+        private DB() {
+            connection = new SqliteConnection("Data Source=../../data/chirp.db");
+            connection.Open();
+        }
+
+        public static DB GetInstance() {
+            return instance;
+        }
+
+        public SqliteConnection? GetConnection() {
+            return connection;
+        }
+
+    }
+}
