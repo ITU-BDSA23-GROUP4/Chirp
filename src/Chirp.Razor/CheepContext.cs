327068d2b2ed4f6127f735445fc7d003a0653a6e
SkarpKat <oash@itu.dk>
2023-10-08 22:46:47 +0200

    Small changes

--- src/Chirp.Razor/CheepContext.cs
+++ src/Chirp.Razor/CheepContext.cs
@@ -2,16 +2,18 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 
-public class CheepContext : DbContext
+namespace CheepDB;
+
+public class ChirpDBContext : DbContext
 {
-    public DbSet<Message>? messages;
-    public DbSet<User>= users;
+    public DbSet<Cheep>? Cheeps;
+    public DbSet<Author>? Authors;
 
     public  string? DbPath { get; }
 
 
 
-    public CheepContext()
+    public ChirpDBContext()
     {
         if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("CHIRPDBPATH")))
         {
@@ -26,21 +28,21 @@ public class CheepContext : DbContext
         => options.UseSqlite($"Data Source={DbPath}");
 }
 
-public class User
+public class Author
 {
-    public int UserId { get; set; }
-    public required string UserName { get; set; }
+    public int AuthorId { get; set; }
+    public required string Name { get; set; }
     public required string Email { get; set; }
-    public required string PWHash { get; set; }
+    public required List<Cheep> Cheeps;
 
 }
 
-public class Message
+public class Cheep
 {
-    public int MessageId { get; set; }
-    public int AuthorId { get; set; }
-    public required string text { get; set; }
-    public required string pubDate { get; set; }
+    public int CheepId { get; set; }
+    public required Author Author { get; set; }
+    public required string Text { get; set; }
+    public required DateTime TimeStamp { get; set; }
 
-    public required User Author { get; set; }
+    
 }
