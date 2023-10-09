555140505a0feb33a5152189add9b06cfeb25c13
SkarpKat <oash@itu.dk>
2023-10-09 22:33:28 +0200

    Added some methodes to add cheeps and users to CheepRepository

--- src/Chirp.Razor/CheepRepository.cs
+++ src/Chirp.Razor/CheepRepository.cs
@@ -27,11 +27,21 @@ public class CheepRepository
         DbInitializer.SeedDatabase(db);
     }
 
+    public void AddAuthor(string name, string email)
+    {
+        db.Add(new Author {Name = name, Cheeps = new List<Cheep>(), Email = email});
+    }
+
+    public void AddCheep(int authorId, string text)
+    {
+        db.Add(new Cheep { Author = db.Authors.Where(author => author.AuthorId == authorId).First(), Text = text, TimeStamp = DateTime.Now });
+    }
+
     public List<CheepViewModel> GetCheeps(int? pageNum)
     {
         List<CheepViewModel> cheepsToReturn = new List<CheepViewModel>();
 
-        var cheeps = db.Cheeps.OrderByDescending(c=>c.TimeStamp.Ticks).Select(cheep => new CheepViewModel(
+        var cheeps = db.Cheeps.OrderByDescending(c => c.TimeStamp.Ticks).Select(cheep => new CheepViewModel(
             cheep.CheepId,
             cheep.Author.Name,
             cheep.Text,
@@ -58,7 +68,7 @@ public class CheepRepository
     {
         List<CheepViewModel> cheepsToReturn = new List<CheepViewModel>();
 
-        var cheeps = db.Cheeps.OrderByDescending(c=>c.TimeStamp.Ticks)
+        var cheeps = db.Cheeps.OrderByDescending(c => c.TimeStamp.Ticks)
             .Where(cheep => cheep.Author != null && cheep.Author.Name != null && cheep.Author.Name.Equals(author))
             .Select(cheep => new CheepViewModel(
                 cheep.CheepId,
