18b84a1beee71c7894beaa5e878304416c0eae3c
SkarpKat <oash@itu.dk>
2023-10-13 21:23:28 +0200

    Added an AuthorDTO with an AuthorRepository

---
+++ src/Chirp.Core/AuthorDTO.cs
@@ -0,0 +1,8 @@
+namespace Chirp.Core;
+
+public class AuthorDTO
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required List<CheepDTO> Cheeps { get; set; }
+}
