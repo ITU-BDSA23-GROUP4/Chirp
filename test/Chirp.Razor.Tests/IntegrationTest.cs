d2b1585a5f53e029c78bc7a1780058fb98a0b343
Marius Thomsen <mariu@itu.dk>
2023-10-14 10:40:24 +0200

    Combined similar tests I altered them from [Fact] to [Theory] I combined CheepsOnPage1IsNotTheSameAsPage2  with the Author version I combined Check32CheepsOnPage2 with the author version Does not pass

--- test/Chirp.Razor.Tests/IntegrationTest.cs
+++ test/Chirp.Razor.Tests/IntegrationTest.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 using System.Text.RegularExpressions;
@@ -36,94 +35,13 @@ public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
         Assert.Contains("Chirp!", content);
         Assert.Contains($"{author}'s Timeline", content);
     }
-    [Fact]
-    public async void Check32Cheeps()
-    {
-        var response = await _client.GetAsync("/");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        //https://stackoverflow.com/questions/3016522/count-the-number-of-times-a-string-appears-within-a-string
-        MatchCollection matches = Regex.Matches(content, "<li>");
-        int count = matches.Count;
-        Assert.Equal(32, count);
-    }
-
-    [Fact]
-    public async void CheepsOnPage0IsTheSameAsPage1()
-    {
-        //Content from standard page: Localhost/
-        var response = await _client.GetAsync("/");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        //Content from page 1
-        var responseFromPageOne = await _client.GetAsync("/?page=1");
-        responseFromPageOne.EnsureSuccessStatusCode();
-        var contentFromPageOne = await responseFromPageOne.Content.ReadAsStringAsync();
-        
-        Assert.Contains(contentFromPageOne, content);
-    }
-
-    [Fact]
-    public async void CheepsOnPage1IsNotTheSameAsPage2()
-    {
-        //Content from standard page 1
-        var response = await _client.GetAsync("/?page=2");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        //Content from page 2
-        var responseFromPageTwo = await _client.GetAsync("/?page=1");
-        responseFromPageTwo.EnsureSuccessStatusCode();
-        var contentFromPageTwo = await responseFromPageTwo.Content.ReadAsStringAsync();
-
-        Assert.NotEqual(contentFromPageTwo, content);
-    }
-
-}
-=======
-using Microsoft.AspNetCore.Mvc.Testing;
-using Xunit;
-using System.Text.RegularExpressions;
-public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
-{
-    private readonly WebApplicationFactory<Program> _fixture;
-    private readonly HttpClient _client;
-
-    public TestAPI(WebApplicationFactory<Program> fixture)
-    {
-        _fixture = fixture;
-        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
-    }
-
-    [Fact]
-    public async void CanSeePublicTimeline()
-    {
-        var response = await _client.GetAsync("/");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains("Public Timeline", content);
-    }
-
+    
     [Theory]
-    [InlineData("Helge")]
-    [InlineData("Rasmus")]
-    public async void CanSeePrivateTimeline(string author)
+    [InlineData("/")]
+    [InlineData("/Jacqualine Gilcoine/")]
+    public async void Check32Cheeps(string path)
     {
-        var response = await _client.GetAsync($"/{author}");
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-
-        Assert.Contains("Chirp!", content);
-        Assert.Contains($"{author}'s Timeline", content);
-    }
-    [Fact]
-    public async void Check32Cheeps()
-    {
-        var response = await _client.GetAsync("/");
+        var response = await _client.GetAsync(path);
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync();
 
@@ -132,19 +50,21 @@ public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
         int count = matches.Count;
         Assert.Equal(32, count);
     }
-     [Fact]
-    public async void Check32CheepsOnPage2()
-    {
-        //arange 
-        var response = await _client.GetAsync("/?page=2");
-        response.EnsureSuccessStatusCode();
-        //Act
-        var content = await response.Content.ReadAsStringAsync();
-        MatchCollection matches = Regex.Matches(content, "<li>");
-        int count = matches.Count;
-        //Assert
-        Assert.Equal(32, count);
-    }
+    // [Theory]
+    // [InlineData("/")]
+    // [InlineData("/Jacqualine Gilcoine/")]
+    // public async void Check32CheepsOnPage2(string path)
+    // {
+    //     //arange 
+    //     var response = await _client.GetAsync(path+"?page=2");
+    //     response.EnsureSuccessStatusCode();
+    //     //Act
+    //     var content = await response.Content.ReadAsStringAsync();
+    //     MatchCollection matches = Regex.Matches(content, "<li>");
+    //     int count = matches.Count;
+    //     //Assert
+    //     Assert.Equal(32, count);
+    // }
     
     [Fact]
     public async void CheepsOnPage0IsTheSameAsPage1()
@@ -161,63 +81,26 @@ public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
         
         Assert.Contains(contentFromPageOne, content);
     }
-
-    [Fact]
-    public async void CheepsOnPage1IsNotTheSameAsPage2()
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/Jacqualine Gilcoine/")]
+    public async void CheepsOnPage1IsNotTheSameAsPage2(string path)
     {
         //Content from standard page 1
-        var response = await _client.GetAsync("/?page=2");
+        var response = await _client.GetAsync(path+"?page=2");
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync();
 
         //Content from page 2
-        var responseFromPageTwo = await _client.GetAsync("/?page=1");
+        var responseFromPageTwo = await _client.GetAsync(path+"?page=1");
         responseFromPageTwo.EnsureSuccessStatusCode();
         var contentFromPageTwo = await responseFromPageTwo.Content.ReadAsStringAsync();
 
         Assert.NotEqual(contentFromPageTwo, content);
     }
-    [Fact]
-    public async void AutherHas32Cheeps(){
-        //Arrange  
-        var response = await _client.GetAsync("/Jacqualine Gilcoine/");
-        response.EnsureSuccessStatusCode();
-        //Act 
-        var content = await response.Content.ReadAsStringAsync();
 
-        MatchCollection matches = Regex.Matches(content, "<li>");
-        int count = matches.Count;
-        //Assert
-        Assert.Equal(32, count);
-     }
-    [Fact]
-    public async void AutherHas32CheepsPage2(){
-        //Arrange  
-        var response = await _client.GetAsync("/Jacqualine Gilcoine/?page=2");
-        response.EnsureSuccessStatusCode();
-        //Act 
-        var content = await response.Content.ReadAsStringAsync();
-
-        MatchCollection matches = Regex.Matches(content, "<li>");
-        int count = matches.Count;
-        //Assert
-        Assert.Equal(32, count);
-     }
-    [Fact]
-    public async void CheepsOnPage1IsNotTheSameAsPage2Author(){
-        //Arrange  
-        //Page 1
-        var response_Page1 = await _client.GetAsync("/Jacqualine Gilcoine/?page=1");
-        response_Page1.EnsureSuccessStatusCode();
-        //Page 2
-        var response_Page2 = await _client.GetAsync("/Jacqualine Gilcoine/?page=2");
-        response_Page2.EnsureSuccessStatusCode();
-        //Act 
-        var content_Page1 = await response_Page1.Content.ReadAsStringAsync();
-        var content_Page2 = await response_Page2.Content.ReadAsStringAsync();
-        //Assert
-        Assert.NotEqual(content_Page1, content_Page2);
-    }
+  
+   
     [Fact]
     public async void CheepsOnPage1ITheSameAsRootAuthor(){
         //Arrange  
@@ -235,4 +118,3 @@ public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
         Assert.Contains(content_Root, content_Page1);
     }
 }
->>>>>>> Stashed changes
