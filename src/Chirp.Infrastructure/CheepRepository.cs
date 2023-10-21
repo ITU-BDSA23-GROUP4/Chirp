e16c126c0bc3d7ba560711b902d890b489ea10db
SkarpKat <oash@itu.dk>
2023-10-21 12:15:12 +0200

    Made it so the CheepRepo follows CQS

--- src/Chirp.Infrastructure/CheepRepository.cs
+++ src/Chirp.Infrastructure/CheepRepository.cs
@@ -6,7 +6,7 @@ namespace Chirp.Infrastructure;
 public class CheepRepository
 {
     private readonly ChirpDBContext db; //Needed to get our CheepDTO
-
+    private AuthorRepository AuthorRepository = new AuthorRepository(); //Needed to get our AuthorDTO
 
 
     public CheepRepository() //Initializes our model
@@ -21,44 +21,34 @@ public class CheepRepository
 
     public void AddCheep(int authorId, string text) 
     {
-        db.Add(new Cheep { Author = db.Authors.
-            Where(author => author.AuthorId == authorId).First(),
+        var author = AuthorRepository.GetAuthorByID(authorId);
+        db.Add(new Cheep { Author = new Author {AuthorId = author.AuthorId, Name = author.Name, Email = author.Email, Cheeps = new List<Cheep>() },
                 Text = text,
                 TimeStamp = DateTime.Now });
+        
     }
 
-    public List<CheepDTO> GetCheeps(int? pageNum)
+    public List<CheepDTO> GetCheeps()
     {
         //Creates a list of max 32 CheepDTO sorted by recent cheep
 
         List<CheepDTO> cheepsToReturn = new List<CheepDTO>();
 
-        var cheepsDTO = db.Cheeps.OrderByDescending(c => c.TimeStamp.Ticks).Select(CheepDTO => new CheepDTO{
+        var cheeps = db.Cheeps.OrderByDescending(c => c.TimeStamp.Ticks);
+        var cheepDTOList = cheeps.Select(cheep => new CheepDTO
+        {
             //Sets the properties of the Cheep
-            AuthorId = CheepDTO.Author.AuthorId,
-            Author = CheepDTO.Author.Name,
-            Message = CheepDTO.Text,
-            Timestamp = CheepDTO.TimeStamp.ToString()
-            }
-        );
+            Author = cheep.Author.Name,
+            Message = cheep.Text,
+            Timestamp = cheep.TimeStamp
+        });
+        cheepsToReturn.AddRange(cheepDTOList);
 
-        cheepsToReturn.AddRange(cheepsDTO);
-
-        int? page = (pageNum - 1) * 32;
-
-        if (page == null)
-        {
-            return cheepsToReturn.GetRange(0, 32);
-        }
-        else
-        {
-            return cheepsToReturn.GetRange((int)page, (int)(page + 32));
-        }
+        return cheepsToReturn;
 
 
     }
-
-    public List<CheepDTO> GetCheepsFromAuthor(string author, int? pageNum)
+    public List<CheepDTO> GetCheepsFromAuthor(string author)
     {
         //Creates a list of max 32 CheepDTO sorted by recent cheep and only for the given author
         
@@ -68,27 +58,49 @@ public class CheepRepository
             .Where(cheep => cheep.Author != null && cheep.Author.Name != null && cheep.Author.Name.Equals(author))
             .Select(CheepDTO => new CheepDTO{
                 //Sets the properties of the Cheep
-                AuthorId = CheepDTO.Author.AuthorId,
                 Author = CheepDTO.Author.Name,
                 Message = CheepDTO.Text,
-                Timestamp = CheepDTO.TimeStamp.ToString()
+                Timestamp = CheepDTO.TimeStamp
             }
         );
         cheepsToReturn.AddRange(cheepsDTO);
 
+        return cheepsToReturn;
+    }
+    
+    public List<CheepDTO> Pagination(int? pageNum)
+    {
         int? page = (pageNum - 1) * 32;
 
 
-        if(cheepsToReturn.Count < 32){
-            return cheepsToReturn.GetRange(0, cheepsToReturn.Count);
+        if(GetCheeps().Count < 32){
+            return GetCheeps().GetRange(0, GetCheeps().Count);
         }
         if (page == null)
         {
-            return cheepsToReturn.GetRange(0, 32);
+            return GetCheeps().GetRange(0, 32);
         }
         else
         {
-            return cheepsToReturn.GetRange((int)page, (int)(page + 32));
+            return GetCheeps().GetRange((int)page, (int)(page + 32));
+        }
+    }
+
+    public List<CheepDTO> Pagination(string author,int? pageNum)
+    {
+        int? page = (pageNum - 1) * 32;
+
+
+        if(GetCheepsFromAuthor(author).Count < 32){
+            return GetCheepsFromAuthor(author).GetRange(0, GetCheepsFromAuthor(author).Count);
+        }
+        if (page == null)
+        {
+            return GetCheepsFromAuthor(author).GetRange(0, 32);
+        }
+        else
+        {
+            return GetCheepsFromAuthor(author).GetRange((int)page, (int)(page + 32));
         }
     }
 }
