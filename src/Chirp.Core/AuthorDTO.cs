3c4f090fb32e2b7d639878fa21a5577c9f657de0
SkarpKat <oash@itu.dk>
2023-10-21 12:13:32 +0200

    removed some of the variables since som may be redundent

--- src/Chirp.Core/AuthorDTO.cs
+++ src/Chirp.Core/AuthorDTO.cs
@@ -2,7 +2,7 @@ namespace Chirp.Core;
 
 public class AuthorDTO
 {
-    public required int AuthorId { get; set; }
+    public int AuthorId { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required List<CheepDTO> Cheeps { get; set; }
