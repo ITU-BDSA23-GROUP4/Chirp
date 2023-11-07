8d400ae1a367cfb05d79d2dc91ee0a89457185b7
Anna <adoringbella2023@gmail.com>
2023-11-07 12:56:05 +0100

    Added the last tests
    All the tests are passing and working

--- test/Chirp.Infrastructure.Tests/CheepRepositoryCreateTest.cs
+++ test/Chirp.Infrastructure.Tests/CheepRepositoryCreateTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
 
 public class CheepRepositoryUnitTests
 {
@@ -8,7 +9,6 @@ public class CheepRepositoryUnitTests
     private readonly ChirpDBContext _context; //Context for the database
     private readonly CheepRepository repository; //Repository for the database
     private readonly CheepCreateValidator validator; //Validator for the database
-    private readonly CheepCreateDTO cheepCreateDto; //DTO for the database
 
     public CheepRepositoryUnitTests()
     {
@@ -32,8 +32,7 @@ public class CheepRepositoryUnitTests
             };
 
         context.Authors.Add(testAuthor); 
-
-        cheepCreateDto = new("TestAuthor", "TestMessage");
+        
         validator = new CheepCreateValidator();
         if (validator == null)
             {
@@ -48,6 +47,10 @@ public class CheepRepositoryUnitTests
     [Fact]
     public async Task UnitTestCreateMethod()
     {
+        //Arrange
+        string Message = "TestMessage";
+        CheepCreateDTO cheepCreateDto = new CheepCreateDTO("TestAuthor", Message);
+
         //Act
         Cheep result = await repository.Create(cheepCreateDto);
 
@@ -55,5 +58,51 @@ public class CheepRepositoryUnitTests
         Assert.NotNull(result);
         Assert.Equal("TestAuthor", result.Author.Name);
         Assert.Equal("TestMessage", result.Text);
+    }
+
+    [Fact]
+    public async Task UnitTestCreateMethodAddedCheepIsInDatabase()
+    {
+        //Arrange
+        var initialCount = _context.Cheeps.Count();
+        string Message = "TestMessage2";
+        CheepCreateDTO cheepCreateDto = new CheepCreateDTO("TestAuthor", Message);
+
+        //Act
+        Cheep result = await repository.Create(cheepCreateDto);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal("TestAuthor", result.Author.Name); 
+        Assert.Equal("TestMessage2", result.Text);
+        Assert.Equal(initialCount+1, _context.Cheeps.Count()); //Checks if the cheep is added to the database
+    }
+
+    [Fact]
+    public void UnitTestCreateMethodAddedCheepShouldNotBeInDatabase1()
+    {
+        //Arrange
+        string Message = "";
+        CheepCreateDTO cheepCreateDto = new CheepCreateDTO("TestAuthor", Message);
+
+        Func<Task> act = async () => await repository.Create(cheepCreateDto); //Adds the cheep to the database
+
+        //Assert
+        //Should throw an exception to pass
+        act.Should().ThrowAsync<ValidationException>().WithMessage("System.ComponentModel.DataAnnotations.ValidationException : Exception of type 'System.ComponentModel.DataAnnotations.ValidationException' was thrown.");
+    }
+
+    [Fact]
+    public void UnitTestCreateMethodAddedCheepShouldNotBeInDatabase2()
+    {
+        //Arrange
+        string Message = "This string should be way over 160 characters, just so we can check that its not possible to make a message that is longer than nessesary.This will because of that, become a very long message.";
+        CheepCreateDTO cheepCreateDto = new CheepCreateDTO("TestAuthor", Message);
+
+        Func<Task> act = async () => await repository.Create(cheepCreateDto); //Adds the cheep to the database
+
+        //Assert
+        //Should throw an exception to pass
+        act.Should().ThrowAsync<ValidationException>().WithMessage("System.ComponentModel.DataAnnotations.ValidationException : Exception of type 'System.ComponentModel.DataAnnotations.ValidationException' was thrown.");
     }
 }
