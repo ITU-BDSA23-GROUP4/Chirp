de77106f9e062a5a093ac0c086fe5bb980dabe72
Oliver <oash@itu.dk>
2023-11-09 14:55:51 +0100

    Remade program to seed the scooped database

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -5,29 +5,37 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Chirp.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddMvc().AddRazorPagesOptions(options =>
 {
     options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
-    
+
 });
 
-builder.Services.AddDbContext<ChirpDBContext>(options => {
+builder.Services.AddDbContext<ChirpDBContext>(options =>
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("ChirpDB"));
 });
 
-builder.Services.AddScoped<AuthorRepository>();
-builder.Services.AddScoped<CheepRepository>();
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+builder.Services.AddScoped<ICheepService, CheepService>();
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
 
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
 var dbContext = services.GetRequiredService<ChirpDBContext>();
 DbInitializer.SeedDatabase(dbContext);
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
