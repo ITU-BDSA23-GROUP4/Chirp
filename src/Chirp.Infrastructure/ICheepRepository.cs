cb39dc4e0999d88a54f3fdc6a8ade43588391c09
Oliver <oash@itu.dk>
2023-11-14 13:25:21 +0100

    Moved interface from core to infastructure

---
+++ src/Chirp.Infrastructure/ICheepRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Chirp.Infrastructure;
+
+namespace Chirp.Core
+{
+    public interface ICheepRepository
+    {
+        void AddCheep(int authorId, string text);
+        List<CheepDTO> GetCheeps(int? pageNum);
+        List<CheepDTO> GetCheepsFromAuthor(string author, int? pageNum);
+        int GetCountOfAllCheeps();
+        int GetCountOfAllCheepFromAuthor(string author);
+        
+    }
+}
