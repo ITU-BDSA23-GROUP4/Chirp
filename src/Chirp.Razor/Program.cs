4cc0e2cf47a749ec4d62ce02d9067949898309d8
Lukas Andersson <lukan@itu.dk>
2023-11-14 13:54:12 +0100

    Now UseAuthentication and authorization is only added when not in prodcution

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -43,6 +43,8 @@ if (!app.Environment.IsDevelopment())
         app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseAuthentication();
+    app.UseAuthorization();
 }
 else 
 {
@@ -60,8 +62,6 @@ app.UseStaticFiles();
 app.UseCookiePolicy();
 app.UseRouting();
 // Add the ASP.NET Core authentication service
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.UseRouting();
 
