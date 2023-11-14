3d3119556c1f890ed39cf125456b95f0d9b27bcf
Lukas Andersson <lukan@itu.dk>
2023-11-14 13:56:08 +0100

    Didn't work

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -43,8 +43,6 @@ if (!app.Environment.IsDevelopment())
         app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseAuthentication();
-    app.UseAuthorization();
 }
 else 
 {
@@ -62,6 +60,8 @@ app.UseStaticFiles();
 app.UseCookiePolicy();
 app.UseRouting();
 // Add the ASP.NET Core authentication service
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseRouting();
 
