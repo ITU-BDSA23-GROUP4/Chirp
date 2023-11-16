8fa87376877564f9e8f9bb195c9ec191e6dbbc6f
Niels Christian Skov Faber <nfab@itu.dk>
2023-11-16 18:10:19 +0100

    Added launch settings and cleaned up program

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -1,29 +1,24 @@
 using Initializer;
 using Chirp.Infrastructure;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddMvc().AddRazorPagesOptions(options =>
-{
-    options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
-    
-});
+builder.Environment.EnvironmentName = "Development";
 
 var connectionString = $"Data source={Path.Combine(Path.GetTempPath() + "chirp.db")}";
 builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
 
 builder.Services.AddScoped<AuthorRepository>();
 builder.Services.AddScoped<CheepRepository>();
-builder.Configuration.AddJsonFile("appSettings.json", optional: false, reloadOnChange: true).AddJsonFile($"appSettings.{builder.Environment.EnvironmentName}.json", optional: true);
+// builder.Configuration.AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+//     .AddJsonFile($"appSettings.{builder.Environment.EnvironmentName}.json", optional: true);
 
 
 using (var context = new ChirpDBContext())
@@ -34,24 +29,15 @@ using (var context = new ChirpDBContext())
 
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
-//builder.Services.AddRazorPages().AddMicrosoftIdentityUI();
-builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-builder.Services.AddRazorPages();
-
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     IdentityModelEventSource.ShowPII = true;
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 else 
@@ -66,19 +52,10 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-
-// Add the Microsoft Identity Web cookie policy
-app.UseCookiePolicy();
 app.UseRouting();
 // Add the ASP.NET Core authentication service
-app.UseAuthorization();
 app.UseAuthentication();
-
-app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseAuthorization();
 
 app.MapRazorPages();
 
