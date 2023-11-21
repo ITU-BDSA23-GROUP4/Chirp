64de0876971278c5ffec2fe77d53e9fb0595de8c
Oliver <oash@itu.dk>
2023-11-21 11:53:34 +0100

    Changes to Program.cs add the fluidvalidation to scope

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -2,18 +2,16 @@ using Initializer;
 using Chirp.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Chirp.Core;
-=======
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using Microsoft.IdentityModel.Logging;
->>>>>>> main
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,54 +24,44 @@ builder.Services.AddMvc().AddRazorPagesOptions(options =>
 
 });
 
-<<<<<<< HEAD
-builder.Services.AddDbContext<ChirpDBContext>(options =>
-=======
-var connectionString = $"Data source={Path.Combine(Path.GetTempPath() + "chirp.db")}";
-builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
-
-builder.Services.AddScoped<AuthorRepository>();
-builder.Services.AddScoped<CheepRepository>();
 builder.Configuration.AddJsonFile("appSettings.json", optional: false, reloadOnChange: true).AddJsonFile($"appSettings.{builder.Environment.EnvironmentName}.json", optional: true);
 
 
-using (var context = new ChirpDBContext())
->>>>>>> main
+builder.Services.AddDbContext<ChirpDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ChirpDB"));
 });
 
+builder.Services.AddScoped<AbstractValidator<CheepCreateDTO>, CheepCreateValidator>();
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 builder.Services.AddScoped<ICheepService, CheepService>();
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
-
-
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
-//builder.Services.AddRazorPages().AddMicrosoftIdentityUI();
-builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-builder.Services.AddRazorPages();
 
 
 var app = builder.Build();
 
-<<<<<<< HEAD
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var dbContext = services.GetRequiredService<ChirpDBContext>();
 DbInitializer.SeedDatabase(dbContext);
 
+// builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+//     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+// //builder.Services.AddRazorPages().AddMicrosoftIdentityUI();
+// builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+
+// builder.Services.AddAuthorization(options =>
+// {
+//     // By default, all incoming requests will be authorized according to the default policy
+//     options.FallbackPolicy = options.DefaultPolicy;
+// });
+// builder.Services.AddRazorPages();
+
+
+
+
 
 // Configure the HTTP request pipeline.
-=======
->>>>>>> main
 if (!app.Environment.IsDevelopment())
 {
     IdentityModelEventSource.ShowPII = true;
