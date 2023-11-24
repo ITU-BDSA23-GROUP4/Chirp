92c43eed5d1cf39442346c00eac32b08eb015c79
Lukas Andersson <lukan@itu.dk>
2023-11-24 19:48:53 +0100

    Removed warnings from Author.cs

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -5,16 +5,16 @@ public class Author
     public int AuthorId { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
-    public List<Cheep> Cheeps;
-    public List<Follow> Followed { get; set;}
-    public List<Follow> Followers { get; set;} 
+    public List<Cheep> Cheeps = new List<Cheep>();
+    public List<Follow> Followed { get; set;} = new List<Follow>();
+    public List<Follow> Followers { get; set;} = new List<Follow>();
 }
 
 public class Follow
 {
     public int FollowId { get; set; }
     public int FollowerId { get; set; }
-    public Author Follower { get; set; }
+    public required Author Follower { get; set; }
     public int FolloweeId { get; set; }
-    public Author Followee { get; set; }
+    public required Author Followee { get; set; }
 }
