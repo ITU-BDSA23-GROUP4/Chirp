0a09cfa1affd04c7329af940cb7f1ae9acbb9d7c
SkarpKat <oash@itu.dk>
2023-12-20 16:22:33 +0100

    Made extra test which required more test data

--- test/Chirp.Infrastructure.Tests/CheepRepositoryUnitTests.cs
+++ test/Chirp.Infrastructure.Tests/CheepRepositoryUnitTests.cs
@@ -33,9 +33,47 @@ public class CheepRepositoryUnitTests {
             Likes = 0,
             TimeStamp = DateTime.Now
         };
+        
+        var testAuthor2 = new Author {
+            AuthorId = new Guid(), 
+            Name = "TestAuthor2", 
+            Email = "TestEmail2", 
+            Cheeps = new List<Cheep>(),
+        };
+        var testCheep2 = new Cheep {
+            CheepId = new Guid(2,0,0,new byte[]{0,0,0,0,0,0,0,0}),
+            Author = testAuthor2,
+            Text = "This is a test2",
+            Likes = 0,
+            TimeStamp = DateTime.Now
+        };
+
+        var testAuthor3 = new Author {
+            AuthorId = new Guid(3,0,0,new byte[]{0,0,0,0,0,0,0,0}), 
+            Name = "TestAuthor3", 
+            Email = "TestEmail3", 
+            Cheeps = new List<Cheep>(),
+        };
+        var testCheep3 = new Cheep {
+            CheepId = new Guid(3,0,0,new byte[]{0,0,0,0,0,0,0,0}),
+            Author = testAuthor3,
+            Text = "This is a test3",
+            Likes = 0,
+            TimeStamp = DateTime.Now
+        };
+
+        testAuthor.Followed = new List<Author>
+        {
+            testAuthor2
+        };
+       
 
         context.Authors.Add(testAuthor); 
         context.Cheeps.Add(testCheep);
+        context.Authors.Add(testAuthor2);
+        context.Cheeps.Add(testCheep2);
+        context.Authors.Add(testAuthor3);
+        context.Cheeps.Add(testCheep3);
         context.SaveChanges();
 
         _context = context;
@@ -63,5 +101,72 @@ public class CheepRepositoryUnitTests {
 
         // Assert
         cheep.Likes.Should().Be(2);
+    }
+
+    //Test get cheeps method
+    [Fact]
+    public void UnitTestGetCheepsFromAuthorReturnsCorrectCheep() 
+    {
+        // Act
+        List<CheepDTO> cheep = repository.GetCheeps(null);
+
+        // Assert
+        cheep.Should().Contain(c => c.AuthorName == "TestAuthor" && c.Message == "This is a test");
+    }
+
+    //Test get cheeps method with author
+    [Fact]
+    public void UnitTestGetCheepsFromAuthorReturnsCorrectCheepWithAuthor() 
+    {
+        // Act
+        List<CheepDTO> cheep = repository.GetCheepsFromAuthor("TestAuthor", null);
+
+        // Assert
+        cheep.Should().Contain(c => c.AuthorName == "TestAuthor" && c.Message == "This is a test");
+    }
+
+    // Test get count of all cheeps
+    [Fact]
+    public void UnitTestGetCountOfAllCheepsReturnsCorrectCount() 
+    {
+        // Act
+        int count = repository.GetCountOfAllCheeps();
+
+        // Assert
+        count.Should().Be(3);
+    }
+
+    // Test get count of all cheeps from author
+    [Fact]
+    public void UnitTestGetCountOfAllCheepsFromAuthorReturnsCorrectCount() 
+    {
+        // Act
+        int count = repository.GetCountOfAllCheepFromAuthor("TestAuthor");
+
+        // Assert
+        count.Should().Be(1);
+    }
+
+    // Test CombineCheepsAndFollowerCheeps
+    [Fact]
+    public void UnitTestCombineCheepsAndFollowerCheepsReturnsCorrectCheep() 
+    {
+        // Act
+        List<CheepDTO> cheep = repository.CombineCheepsAndFollowerCheeps("TestAuthor", null);
+
+        // Assert
+        cheep.Should().Contain(c => c.AuthorName == "TestAuthor" && c.Message == "This is a test");
+        cheep.Should().Contain(c => c.AuthorName == "TestAuthor2" && c.Message == "This is a test2");
+    }
+
+    // Test GetCountOfAllCheepsFromCombinedAuthor with author
+    [Fact]
+    public void UnitTestGetCountOfAllCheepsFromCombinedAuthorReturnsCorrectCount() 
+    {
+        // Act
+        int count = repository.GetCountOfAllCheepsFromCombinedAuthor("TestAuthor");
+
+        // Assert
+        count.Should().Be(2);
     }
 }
