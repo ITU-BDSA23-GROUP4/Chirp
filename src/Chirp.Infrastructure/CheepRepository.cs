4780b6e6b8a0cf38c692a2a007d54c11a908a93c
Lukas Andersson <lukan@itu.dk>
2023-12-20 01:42:33 +0100

    Removed trivial try-catch from DeleteCheepsFromAuthor in CheepsRepository. The try-catch statement is trivial, since the catch just throws the exception again.

--- src/Chirp.Infrastructure/CheepRepository.cs
+++ src/Chirp.Infrastructure/CheepRepository.cs
@@ -53,18 +53,11 @@ public class CheepRepository : ICheepRepository
     }
 
     public async Task DeleteCheepsFromAuthor(Guid authorid){
-        //Deletes all cheeps from a given author
-        try
-        {
-            var author = GetAuthorById(authorid); //Gets the author from the database
-            if(author != null){ //Deletes all the cheeps from the author
-                _db.RemoveRange(_db.Cheeps.Where(cheep => cheep.Author == author));
-                await _db.SaveChangesAsync();
-            }
-        }
-        catch (System.Exception)
-        {
-            throw;
+        //Deletes all cheeps from a given author        
+        var author = GetAuthorById(authorid); //Gets the author from the database
+        if(author != null){ //Deletes all the cheeps from the author
+            _db.RemoveRange(_db.Cheeps.Where(cheep => cheep.Author == author));
+            await _db.SaveChangesAsync();
         }
     }
 
